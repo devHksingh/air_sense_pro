fe7d2aa3169981138db48e4f214d10afb867fd0c
devHksingh <hksingh.dev@gmail.com>
2026-07-27 23:08:33 +0530

    add setup file

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+import { config as conf } from "dotenv";
+conf();
+
+const config = {
+  port: process.env.PORT || 6000,
+  nodeEnv: process.env.NODE_ENV || "development",
+  mongoUri: process.env.MONGO_URI,
+  MQTT_TOPIC: process.env.MQTT_TOPIC,
+  MQTT_BROKER_HOST: process.env.MQTT_BROKER_HOST,
+  MQTT_USERNAME: process.env.MQTT_USERNAME,
+  MQTT_PASSWORD: process.env.MQTT_PASSWORD,
+  MQTT_BROKER_PORT: process.env.MQTT_BROKER_PORT,
+  MQTT_BROKER_PROTOCOL: process.env.MQTT_BROKER_PROTOCOL,
+
+};
